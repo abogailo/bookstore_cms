4826e92504b77e0173d2ade7fa6668105bb88508
abogailo <abagailanne@gmail.com>
2019-10-23 20:30:27 -0400

    start of the views

--- bookstore_cms/app/models/book.rb
+++ bookstore_cms/app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ApplicationRecord
     belongs_to :author
     belongs_to :administrator,
-        :class_name => "User"
-        :foreign_kay => "user_id"
+        class_name: "User",
+        foreign_key: "user_id"
     has_many :categories, through: :category_books, source: :category
     has_many :book_genres, through: :genre_books, source: :genre
     has_many :reviews, dependent: :destroy
--- bookstore_cms/config/routes.rb
+++ bookstore_cms/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "books#index"
   resources :users, :except => [:index]
   resources :sessions, only: [:new, :create]
-  
+  resources :books
+
+
   get '/login' => "sessions#new"
   post '/sessions' => "sessions#create"
   get '/logout' => "sessions#destroy"
